86db3486f69da057bc4689b3c844359b00517e84
Aline F. Brito <alinefbrito@gmail.com>
2022-12-02 00:00:22 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,16 @@
+using System;
+
+class Program {
+  public static void Main (string[] args) {
+    //declara variavel
+    // var nome : caractere
+    string nome;
+    
+    //Leia(nome)
+    nome = Console.ReadLine();
+    
+    
+    //Escreval("Hello World")
+    Console.WriteLine ("Hello " + nome);
+  }
+}
